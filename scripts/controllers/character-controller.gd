dd63068e330db992581985c320a42b8da819ef69
Than <thanthemannn@gmail.com>
2025-07-30 17:54:18 -0400

    fix: just use node as reference for character-controller input handler

--- scripts/controllers/character-controller.gd
+++ scripts/controllers/character-controller.gd
@@ -3,11 +3,10 @@ extends Node2D
 class_name CharacterController
 
 @export var body: CharacterBody2D
-@export var inputHandler: PlayerInput
+@export var inputHandler: Node
 
 @export var speed := 1.0
 
 func _process(delta: float):
 	body.velocity = inputHandler.input.move * delta * speed
-	print(body.velocity)
 	body.move_and_slide()
